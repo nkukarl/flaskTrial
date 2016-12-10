10f0a81cb8ecb0c00ecc024908bde3fb5624d1f5
Kai Wang <kai.wang.nankai@gmail.com>
2016-12-10 11:32:24 +1100

    Add profile and post page!

--- index.py
+++ index.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def index():
     return 'This is the home page!'
 
+@app.route('/profile/<username>')
+def profile(username):
+    return username
+
+@app.route('/post/<int:id>')
+def show(id):
+    return str(id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
