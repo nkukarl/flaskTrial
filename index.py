ca70fdd7a92e06c85d0d7eecc39bb4bcf6bb5841
Kai Wang <kai.wang.nankai@gmail.com>
2016-12-10 15:28:40 +1100

    Map multiple urls

--- index.py
+++ index.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'This is the home page!'
-
-@app.route('/profile/<name>')
-def profile(name):
-    return render_template('profile.html', name=name)
+@app.route('/<user>')
+def index(user=None):
+    return render_template('user.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
