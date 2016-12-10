816fc7460447b300f94279d8bd9be26873eda38b
Kai Wang <kai.wang.nankai@gmail.com>
2016-12-10 15:53:31 +1100

    Display user info

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from user.user import User
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<user>')
-def index(user=None):
-    return render_template('user.html', user=user)
+@app.route('/<username>')
+def index(username=None):
+    if username:
+        user = User(username, 25, ['eating', 'working', 'dancing'])
+        return render_template('user.html', user=user)
+    return render_template('user.html', user=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ user/user.py
@@ -0,0 +1,8 @@
+class User(object):
+    def __init__(self, name, age, hobbies):
+        self.name = name
+        self.age = age
+        self.hobbies = hobbies
+
+    def update(self):
+        pass
