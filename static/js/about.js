e4b8f6052338238e926c7fc0d1600a02a98ccbfd
Kai Wang <kai.wang.nankai@gmail.com>
2016-12-11 17:07:58 +1100

    Add function to js

--- static/js/about.js
+++ static/js/about.js
@@ -1,2 +1,12 @@
 let text = document.getElementById('about');
 text.innerHTML = 'ABOUT';
+
+add = (i) => {
+    let val = document.getElementById('val');
+    val.innerHTML = parseInt(val.innerHTML) + i;
+};
+
+sub = (i) => {
+    let val = document.getElementById('val');
+    val.innerHTML = parseInt(val.innerHTML) - i;
+};
